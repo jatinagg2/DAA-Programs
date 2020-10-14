ca148ed90e3a5372240467cfa39f893f370a7400
Denio Shaji John <44223115+DenioSJ@users.noreply.github.com>
2020-10-14 12:37:44 +0530

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -36,16 +36,13 @@ class Graph
     public static void main(String args[]) 
 	{ 
 		Graph g = new Graph(4); 
-
 		g.addEdge(0, 1); 
 		g.addEdge(0, 2); 
 		g.addEdge(1, 2); 
 		g.addEdge(2, 0); 
 		g.addEdge(2, 3); 
 		g.addEdge(3, 3); 
-
-		System.out.println("Following is Depth First Traversal "+ 
-						"(starting from vertex 2)"); 
+		System.out.println("Following is Depth First Traversal "+ "(starting from vertex 2)"); 
         g.DFS(2); 
 	} 
 } 
